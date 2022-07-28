98cece4eb80c946d204812cc0de8752dc04c259b
Sanja <sanja1mandic@gmail.com>
2022-07-28 13:00:42 +0200

    Sett routes for pages and global state dispath actions for getting APIa and updating the state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import NavBar from './components/Navbar';
 import Characters from './components/Characters';
 import Planets from './components/Planets';
 import Starships from './components/Starships';
+import Films from './components/Films';
+import Detail from './components/Detail';
+import { getCharacters } from './redux/characters';
+import { getPlanets } from './redux/planets';
+import { getFilms } from './redux/films';
+import { getShips } from './redux/starships';
+import Footer from './components/Footer';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getFilms());
+    for (let i = 1; i <= 9; i += 1) {
+      dispatch(getCharacters(i));
+    }
+    for (let i = 1; i <= 6; i += 1) {
+      dispatch(getPlanets(i));
+    }
+    for (let i = 1; i <= 4; i += 1) {
+      dispatch(getShips(i));
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
+    <>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Characters />} />
+        <Route exact path="/" element={<Films />} />
         <Route path="/characters" element={<Characters />} />
         <Route path="/planets" element={<Planets />} />
         <Route path="/starships" element={<Starships />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
-    </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
